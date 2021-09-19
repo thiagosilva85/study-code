3e4957c4e93ac8d71c34357db8296195abcb5283
thiagosilva85 <thiago.silva85@yahoo.com.br>
2021-09-19 18:55:13 -0300

    Create busca-linear.py

---
+++ busca-linear.py
@@ -0,0 +1,19 @@
+import time
+import random
+
+random.seed(42)
+
+sorteio_tamanho_vetor = random.randrange(1000,10000)
+print("Tamanho do vetor: ", sorteio_tamanho_vetor)
+
+A = [random.randint(0,1000) for i in range(sorteio_tamanho_vetor)]
+n = len(A)
+x = 5
+
+def busca_linear(A, n, x):
+    resposta = -1
+    for i in range(0, n):
+        if(A[i] == x): resposta = i
+    return resposta
+
+busca_linear(A, n, x)
